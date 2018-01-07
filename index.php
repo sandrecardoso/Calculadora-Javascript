b3aa3d4374c6da06a1d08defc66dc0e3fb2877c2
Sandré Cardoso <habitsul@gmail.com>
2018-01-07 14:27:19 -0200

    calculadora

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="pt-br">
+<head>
+
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+  <title>Calc</title>
+
+
+</head>
+<body>
+
+<form method="GET" action="resultado.php" name="calculadora">
+Num1: <input name="num1" type="text"><br>
+
+Num2<input name="num2" type="text"><br>
+
+  <input value="soma" type="submit">
+</form>
+
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="pt-br">
+<head>
+
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+  <title>Resultado</title>
+
+
+</head>
+<body style="background-color: gray;">
+
+<?php 
+
+
+
+$soma=$num1+$num2;
+echo "A soma é:$soma";
+?>
+</body>
+</html>
